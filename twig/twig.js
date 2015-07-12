79e5a9e655066f2400747f20b54813c34404d39c
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2015-07-12 21:34:12 +0200

    Twig: twig_render_into(): use twig's xmlhttprequest; cache requests until loaded

--- twig/twig.js
+++ twig/twig.js
@@ -4,27 +4,38 @@ function twig_render_into_final(dom_node, template_id, data, callback, result) {
   if(typeof dom_node == "string")
     dom_node = document.getElementById(dom_node);
 
-  if(!(template_id in template_cache))
-    template_cache[template_id] = twig({ data: result });
-
-  dom_node.innerHTML = template_cache[template_id].render(data);
+  dom_node.innerHTML = twig({ ref: template_id }).render(data);
 
   if(callback)
     callback(dom_node, template_id, data, result);
+
+  // if there are still requests to process, do so
+  if(template_cache[template_id] !== true) {
+    var tc = template_cache[template_id];
+    template_cache[template_id] = true;
+
+    for(var i = 0; i < tc.length; i++) {
+      var t = tc[i];
+      twig_render_into_final(t[0], template_id, t[1], t[2]);
+    }
+  }
 }
 
 function twig_render_into(dom_node, template_id, data, callback) {
-  if(template_id in template_cache) {
+  if(!(template_id in template_cache)) {
+    twig({
+      id: template_id,
+      href: 'templates/' + template_id,
+      load: twig_render_into_final.bind(this, dom_node, template_id, data, callback)
+    });
+
+    template_cache[template_id] = [];
+  }
+  else if(template_cache[template_id] === true) {
     twig_render_into_final(dom_node, template_id, data, callback);
   }
   else {
-    var req = new XMLHttpRequest();
-    req.open('GET', 'templates/' + template_id, true);
-    req.onreadystatechange = function(req, callnext) {
-      if(req.readyState == 4)
-        callnext(req.responseText);
-    }.bind(this, req, twig_render_into_final.bind(this, dom_node, template_id, data, callback));
-    req.send(null);
+    template_cache[template_id].push([ dom_node, data, callback ]);
   }
 }
 
